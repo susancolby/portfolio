6704e820af3e381a8a560750fa69b47fef1b10fb
susancolby <78624571+susancolby@users.noreply.github.com>
2025-12-25 15:52:14 +0100

    Create racing.js

---
+++ racing.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("racing");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "black";
+ctx.font = "30px Arial"
