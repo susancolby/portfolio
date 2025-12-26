65cd81ff38d77d5b54187f2323c2a493e43c0de2
susancolby <78624571+susancolby@users.noreply.github.com>
2025-12-26 21:07:02 +0100

    Update racing.js

--- racing.js
+++ racing.js
@@ -4,12 +4,34 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const player1 = {
+  x: 100,
+  y: canvas.height / 2-50,
+  width: 50,
+  height: 30,
+  color: "red",
+  speed: 0
+};
+
+const player2 = {
+  x: 100,
+  y: canvas.height / 2 +50,
+  width: 50,
+  height: 30,
+  color: "blue",
+  speed: 0
+};
+
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "black";
-  ctx.font = "30px Arial";
-  ctx.fillText("Game running...", 50, 50);
+  
+  ctx.fillStyle = player1.color;
+  ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  ctx.fillStyle = player2.color;
+  ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+  
   requestAnimationFrame(gameLoop);
-}
+};
 
 gameLoop();
